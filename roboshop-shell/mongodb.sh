96a9094df64bb2644866a111e0c2cc396228278f
manoj <manojkumarennam123@gmail.com>
2023-01-04 21:59:02 +0530

    sample

--- roboshop-shell/mongodb.sh
+++ roboshop-shell/mongodb.sh
@@ -1,7 +1,9 @@
+script_location=$(pwd)
 
 yum install mongodb-org -y
 
 systemctl enable mongod
 systemctl start mongod
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
 
 systemctl restart mongod
